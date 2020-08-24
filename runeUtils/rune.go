cf28f29fbc021228e30811f068b23469247bf925
soyacen <soyacen@outlook.com>
2020-08-24 16:09:51 +0800

    add DigitVal

--- runeUtils/rune.go
+++ runeUtils/rune.go
@@ -9,3 +9,15 @@ func IsLetter(ch rune) bool {
 func IsDigit(ch rune) bool {
 	return '0' <= ch && ch <= '9' || ch >= 0x80 && unicode.IsDigit(ch)
 }
+
+func DigitVal(ch rune) int {
+	switch {
+	case '0' <= ch && ch <= '9':
+		return int(ch - '0')
+	case 'a' <= ch && ch <= 'f':
+		return int(ch - 'a' + 10)
+	case 'A' <= ch && ch <= 'F':
+		return int(ch - 'A' + 10)
+	}
+	return 16 // larger than any legal digit val
+}
