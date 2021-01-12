e43536cd0c903d651561ec3928755cc91001c649
soyacen <soyacen@outlook.com>
2021-01-12 15:55:52 +0800

    add Int32SliceJoin

---
+++ sliceutils/int.go
@@ -0,0 +1,24 @@
+package sliceutils
+
+import (
+	"strconv"
+)
+
+// Int32SliceJoin concatenates the elements of its first argument to create a single string. The separator
+// string sep is placed between elements in the resulting string.
+func Int32SliceJoin(elems []int32, sep string) string {
+	switch len(elems) {
+	case 0:
+		return ""
+	case 1:
+		return strconv.Itoa(int(elems[0]))
+	}
+	var bs []byte
+
+	strconv.AppendInt(bs, int64(elems[0]), 10)
+	for _, s := range elems[1:] {
+		bs = append(bs, sep...)
+		strconv.AppendInt(bs, int64(s), 10)
+	}
+	return string(bs)
+}
